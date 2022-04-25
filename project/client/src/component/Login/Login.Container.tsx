26de70b7e9ed0d4f24f802a3b155f919d0a34443
kth7024 <guinness5167@gmail.com>
2022-04-26 07:48:09 +0900

    project setting

---
+++ project/client/src/component/Login/Login.Container.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React, {
+  ChangeEvent,
+  useEffect,
+  useState,
+  MouseEvent,
+  FormEventHandler,
+  FormEvent,
+} from "react";
+import LoginPageUI from "./Login.Presenter";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../_action/user_action";
+import { useNavigate } from "react-router-dom";
+import { withAuth } from "../../hoc/isAuth";
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onClickLoginButton: FormEventHandler<HTMLFormElement> = (
+    e: FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    const body = {
+      email,
+      password,
+    };
+    console.log(body);
+    // @ts-ignore
+    dispatch(loginUser(body)).then((res) => {
+      if (res.payload.loginSuccess) {
+        navigate("/");
+      } else {
+        alert("err");
+      }
+    });
+  };
+
+  const onClickSignUpButton = () => {
+    navigate("/register");
+  };
+
+  return (
+    <LoginPageUI
+      email={email}
+      password={password}
+      onChangeEmail={onChangeEmail}
+      onChangePassword={onChangePassword}
+      onClickLoginButton={onClickLoginButton}
+      onClickSignUpButton={onClickSignUpButton}
+    />
+  );
+}
---
+++ project/client/src/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import { applyMiddleware } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+// 이 부분 블로그
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__: any;
+  }
+}
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
